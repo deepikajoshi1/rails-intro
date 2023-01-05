3ce3df2468a1bf7a6f1ce659c02ede907c308179
deepika joshi <upreti.deepika@gmail.com>
2023-01-05 23:11:51 +0800

    final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,9 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @members = ["aim", "bim", "cim", "dim", "eim"]
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # Let’s add an home page to our app:
-  get "home", to: "pages#home"
+  # get "home", to: "pages#home"
   # usually we write this one : root to: "pages#home"
+  root to: "pages#home"
 
   # Let’s add an about page to our app:
    get "about", to: "pages#about"

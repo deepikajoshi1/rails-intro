86dd6d11cf39cc2d1d278d28d766aa743a758b66
deepika joshi <upreti.deepika@gmail.com>
2023-01-05 16:33:42 +0800

    contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Let’s add an home page to our app:
+  get "home", to: "pages#home"
+  # usually we write this one : root to: "pages#home"
+
   # Let’s add an about page to our app:
-  get "about", to: "pages#about"
+   get "about", to: "pages#about"
+  # We want to add a contact page:
+  get "contact", to: "pages#contact"
 end
